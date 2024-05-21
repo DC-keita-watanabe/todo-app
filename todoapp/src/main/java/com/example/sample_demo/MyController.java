73e235e8baade4133d80fd59e21a11e94e11d06a
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-05-21 18:46:38 +0900

    MyCotroller.javaにタスクを削除するエンドポイントを追加

--- todoapp/src/main/java/com/example/sample_demo/MyController.java
+++ todoapp/src/main/java/com/example/sample_demo/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,10 +31,20 @@ public class MyController {
 	//POSTリクエストを処理し、タスクを追加する
 	@PostMapping("/create")
 	@CrossOrigin(origins = "http://localhost:3000")
-	public void create(@RequestBody TodoValidate todo) {
+	public void create(@RequestBody Todo todo) {
 		System.out.println(todo);
 		todoService.addTodo(todo.getTaskName(), todo.getTaskDescription(), todo.getAssignPersonName(), todo.getEstimateHour());
 	}
+	
+	// POSTリクエストを処理し、タスクを削除する
+	@PostMapping("/delete/{id}")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public void delete(@PathVariable int id) {
+		System.out.println(id);
+		todoService.deleteTodo(id);
+	}
+	
+	
 	
 	
 }
