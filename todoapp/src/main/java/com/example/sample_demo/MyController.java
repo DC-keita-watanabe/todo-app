38f53c81809b898baaf459bdea7e77408cda25bc
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-05-21 15:23:47 +0900

    MyCotroller.javaにタスク表示APIを実装

--- todoapp/src/main/java/com/example/sample_demo/MyController.java
+++ todoapp/src/main/java/com/example/sample_demo/MyController.java
@@ -1,5 +1,21 @@
 package com.example.sample_demo;
 
-public class MyController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@Autowired TodoService todoService;
+	
+	@GetMapping("/api/tasks")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public List<Todo> showTodos(Model model) {
+		List <Todo> todos = todoService.searchAll();
+		return todos;
+	}
 }
