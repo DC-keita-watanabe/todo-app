ca3d0b3dc0173b8751fb85fb820596724305cbfd
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-05-21 15:59:02 +0900

    MyCotroller.javaにタスクを追加するエンドポイントを追加

--- todoapp/src/main/java/com/example/sample_demo/MyController.java
+++ todoapp/src/main/java/com/example/sample_demo/MyController.java
@@ -4,18 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class MyController {
 	@Autowired TodoService todoService;
 	
+	// GETリクエストを処理し、全てのToDoタスクを返す
 	@GetMapping("/api/tasks")
 	@CrossOrigin(origins = "http://localhost:3000")
 	public List<Todo> showTodos(Model model) {
 		List <Todo> todos = todoService.searchAll();
 		return todos;
 	}
+	
+	//POSTリクエストを処理し、タスクを追加する
+	@PostMapping("/create")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public void create(@RequestBody TodoValidate todo) {
+		System.out.println(todo);
+		todoService.addTodo(todo.getTaskName(), todo.getTaskDescription(), todo.getAssignPersonName(), todo.getEstimateHour());
+	}
+	
+	
 }
