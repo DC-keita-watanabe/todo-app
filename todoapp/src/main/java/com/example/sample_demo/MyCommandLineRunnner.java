06e63928fff507c63a36b1c038698107814244f1
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-05-22 14:34:38 +0900

    csvからデータ読み込み処理を追加

--- todoapp/src/main/java/com/example/sample_demo/MyCommandLineRunnner.java
+++ todoapp/src/main/java/com/example/sample_demo/MyCommandLineRunnner.java
@@ -1,5 +1,11 @@
 package com.example.sample_demo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,10 +20,28 @@ public class MyCommandLineRunnner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("コマンドラインランナー実行開始");
         
-        Todo todo = new Todo("タスク1","タスク1です","道長",3.5);
-        todoRepository.save(todo);
+        List<Todo> todos = readTodoFromCSV("historical_figures.csv");
+        todoRepository.saveAll(todos);
         
         System.out.println("コマンドラインランナー実行終了");
+    }
+    
+    private List<Todo> readTodoFromCSV(String fileName) {
+        List<Todo> todos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            br.readLine(); // ヘッダーをスキップ
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                String taskName = fields[0];
+                String taskDescription = fields[1];
+                String assignPersonName = fields[2];
+                todos.add(new Todo(taskName, taskDescription, assignPersonName));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return todos;
     }
 }
 
